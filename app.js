30618e7be3a73285cf43c03304415fbf3e0b4d70
sssnff <nastya.hunter@gmail.com>
2016-02-01 16:54:44 +0200

    new button

--- app.js
+++ app.js
@@ -29,15 +29,21 @@ function addArticleLS(key, payload){
 function onButtonSearch(){
   var key = document.getElementById('js-key').value;
   var payload = searchArticleLS(key);
+  document.getElementById('js-new-key').value = key;
   document.getElementById('js-payload').innerText = payload;
   document.getElementById('js-add-block').style.visibility = payload === EMPTY_PAYLOAD ? 'visible' :'hidden';
 }
 
 function onButtonAdd(){
-  var key = document.getElementById('js-key').value;
+  var key = document.getElementById('js-new-key').value;
   var payload = document.getElementById('js-add').value;
   document.getElementById('js-payload').innerText = payload;
-  document.getElementById('js-add-block').style.visibility = payload ='visible';
   addArticleLS(key, payload);
 }
 
+function onButtonNewAdd(){
+  var key = document.getElementById('js-key').value;
+  document.getElementById('js-new-key').value = key;
+  document.getElementById('js-payload').innerText = "";
+  document.getElementById('js-add-block').style.visibility = 'visible';
+}
